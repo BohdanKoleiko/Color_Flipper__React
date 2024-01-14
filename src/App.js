e32c4e5350c8c6a110c1639c9c90620e48d4cb77
HamaHs <lusuy1995@gmail.com>
2024-01-14 22:59:50 +0100

    finished to create project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,77 @@
-import "./App.css";
+import { useState, useEffect } from "react";
+import styles from "./App.module.css";
+import Button from "./components/Button/Button";
+import Header from "./components/Header/Header";
+
+const colorsList = "html";
+const API_URL = `https://api.color.pizza/v1/?list=${colorsList}`;
 
 function App() {
-   return;
+   const [data, setData] = useState({
+      colors: [],
+      colorType: "name",
+      isLoading: false,
+      error: "",
+   });
+   const [numberCurrentColor, setNumberCurrentColor] = useState(0);
+
+   const setDataHandler = (name, value) => {
+      setData((prevState) => ({ ...prevState, [name]: value }));
+   };
+
+   const setColorTypeHandler = (type) => {
+      setData({ ...data, colorType: type });
+   };
+
+   const generateRandomNumber = () => {
+      setNumberCurrentColor(Math.floor(Math.random() * data.colors.length));
+   };
+
+   const getColor = (name) => {
+      return data.colors[numberCurrentColor][name];
+   };
+
+   useEffect(() => {
+      (async () => {
+         try {
+            const res = await fetch(API_URL);
+            const gottenColors = await res.json();
+
+            setDataHandler("colors", gottenColors.colors);
+         } catch (error) {
+            setDataHandler("error", error.message);
+         }
+
+         setDataHandler("isLoading", true);
+      })();
+   }, []);
+
+   return data.error ? (
+      <h2 className={styles.errorMesage}>{data.error}</h2>
+   ) : (
+      data.isLoading && (
+         <>
+            <Header setColorType={setColorTypeHandler} {...data} />
+            <main
+               className={styles.main}
+               style={{ backgroundColor: `${getColor(data.colorType)}` }}
+            >
+               <div>
+                  <p
+                     className={styles.text}
+                     style={{
+                        backgroundColor: `${getColor("bestContrast")}`,
+                        color: `${getColor(data.colorType)}`,
+                     }}
+                  >
+                     background color: <span>{getColor(data.colorType)}</span>
+                  </p>
+                  <Button randomNumber={generateRandomNumber} getColor={getColor} {...data} />
+               </div>
+            </main>
+         </>
+      )
+   );
 }
 
 export default App;
